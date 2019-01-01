d380a3b8e6c407fb3f9f4c3135fa01aa6e35b5d2
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2019-01-01 16:56:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,6 +40,17 @@ client.on('ready', function(){
  console.log(` ───────────────╚══╝`);
 
 
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "welcome");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
+});
+
 
  client.on('message', message => {
      if (message.content === "bot") {
